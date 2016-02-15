572eb8350358b5f483ac401d4bedf1bb9d848b0b
MichaelSentry <gitsentry@ninjasentry.com>
2016-02-15 16:46:23 +1100

    Update range policy

--- src/policy/range.php
+++ src/policy/range.php
@@ -4,5 +4,7 @@
  * IP Range Policy
  */
 return [
-    '23.95.247.0/24', // reverse proxies ( colocrossing )
+    '23.95.247.0/24',     // reverse proxies ( colocrossing )
+    '37.59.164.96/28',    // perfect privacy ( ovh )
+    '217.114.218.16/28' , // perfect privacy ( keyweb )
 ];
