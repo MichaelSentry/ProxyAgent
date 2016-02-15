c73bc87b718ea4dc6328fea4cddb94374fcf06cf
MichaelSentry <gitsentry@ninjasentry.com>
2016-02-15 13:18:26 +1100

    Update Host detection policy

--- src/policy/host.php
+++ src/policy/host.php
@@ -5,7 +5,12 @@
  * NinjaSentry.com 2016
  *
  * Proxy host detection policy
+ * Last Updated : 15th Feb 2016
  */
 return [
-    ''
+    'anon-online.org',
+    'anonymizer.ccc.de',
+    'perfect-privacy.com',
+    'torservers.net',
+    'privax.com',
 ];
